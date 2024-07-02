51144c4f60ec583fdf0457068b11b8ce7ba7e533
innocent-charles <innocentcharles217@gmail.com>
2024-07-03 02:46:08 +0300

    removed build error

--- app/components/ui/chat/hooks/extract-pdf.ts
+++ app/components/ui/chat/hooks/extract-pdf.ts
@@ -1,17 +1,19 @@
-export async function doExtractPdf(file: File): Promise<any> {
+export const doExtractPdf = async (file: File): Promise<any> => {
     const url = new URL(`${process.env.NEXT_PUBLIC_BASE_URL}/api/chatpdf-text`);
     const formData = new FormData();
     formData.append('file', file, file.name);
 
     try {
-        const response = await fetch(url, {
+        const response = await fetch(url.toString(), {
             method: 'POST',
-            body: formData});
+            body: formData
+        });
 
         if (!response.ok) {
-            alert('Failed to upload PDF file. Please try again.');}
-        return response.json();
+            alert('Failed to upload PDF file. Please try again.');
+        }
+        return await response.json();
     } catch (error) {
         alert('Failed to upload PDF file. Please try again.');
     }
-}
+};
--- app/components/ui/chat/hooks/save-feedbacks.ts
+++ app/components/ui/chat/hooks/save-feedbacks.ts
@@ -1,17 +1,15 @@
-export async function dosaveFeedBacks(data: Record<string, any>): Promise<any> {
-    const url = new URL(`${process.env.NEXT_PUBLIC_CHAT_API}/user-feedbacks`);
-    const headers = new Headers();
-    headers.append("Content-type", "application/json; charset=UTF-8");
-    try{
-        await fetch(url, {
-            method: 'POST',
-            headers: headers,
-            body: JSON.stringify(data),
-          });
+export const dosaveFeedBacks = async (data: Record<string, any>): Promise<any> => {
+  const url = new URL(`${process.env.NEXT_PUBLIC_CHAT_API}/user-feedbacks`);
+  const headers = new Headers({
+      "Content-type": "application/json; charset=UTF-8"
+  });
 
-    }catch(error){}
-
-  }
-
-
-
+  try {
+      const response = await fetch(url.toString(), {
+          method: 'POST',
+          headers: headers,
+          body: JSON.stringify(data),
+      });
+      return await response.json();
+  } catch (error) {}
+};
